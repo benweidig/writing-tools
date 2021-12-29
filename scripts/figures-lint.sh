95edddd7b2574d5cba28b80f5e2b7fedb2abe040
Ben Weidig <ben@netzgut.net>
2021-12-29 12:46:29 +0100

    scripts/figures-lint.sh: check for angle brackets in alt

--- scripts/figures-lint.sh
+++ scripts/figures-lint.sh
@@ -15,12 +15,18 @@ RC=0
 
 for BOOK_FILE in "$1"/*.asciidoc; do
     while IFS=: read -r FIGURE_PATH; do
-      if [[ ! -f "$FIGURE_PATH" ]]; then
-        >&2 printf "%s: figure '%s' is missing.\n" "$BOOK_FILE" "$FIGURE_PATH"
-        RC=1
-    fi
+        if [[ ! -f "$FIGURE_PATH" ]]; then
+            >&2 printf "%s: figure '%s' is missing.\n" "$BOOK_FILE" "$FIGURE_PATH"
+            RC=1
+        fi
     done < <(sed -n 's/image::\(.*\)\[.*\]/\1/p' "$BOOK_FILE")
 
+    while IFS=: read -r FIGURE_PATH_WITH_ALT; do
+        if [[ $FIGURE_PATH_WITH_ALT =~ .*\[\".*(<|>).*\"\] ]]; then
+            >&2 printf "%s: figure '%s' alt contains invalid char.\n" "$BOOK_FILE" "$FIGURE_PATH_WITH_ALT"
+            RC=1
+        fi
+    done < <(sed -n 's/image::\(.*\)/\1/p' "$BOOK_FILE")
 done
 
 exit $RC
