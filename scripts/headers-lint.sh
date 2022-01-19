2a0ea8dfabb25ba7b474f93c8a37302e4d8edf18
Ben Weidig <ben@netzgut.net>
2022-01-19 09:59:18 +0100

    scripts: headers-lint.sh added

---
+++ scripts/headers-lint.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+if [[ -n "$DEBUG" ]]; then
+    set -x
+fi
+
+if [[ ! "$#" -eq 1 ]]; then
+    >&2 printf "1 argument must be provided (book-folder), but found '%s'.\n" "$#"
+    exit 1
+fi
+
+RC=0
+
+for BOOK_FILE in "$1"/*.asciidoc; do
+
+    readarray -t PROBLEMS < <(grep -nE '^=+ .*\+.+\+.*$' "$BOOK_FILE")
+
+    if [[ $? -eq 0 ]]; then
+
+        for PROBLEM in "${PROBLEMS[@]}"; do
+            printf "%s:%s\n" "$BOOK_FILE" "$PROBLEM"
+        done
+
+        RC=1
+    fi
+done
+
+exit $RC
